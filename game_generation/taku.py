adc76e9bbf889e94bd7517073bb0e23a78f166c3
zhuobinggang <zhuobinggang@gmail.com>
2024-06-01 17:51:42 +0900

    dd

--- game_generation/taku.py
+++ game_generation/taku.py
@@ -4,3 +4,24 @@ from twc_make_game import *
 config = twc_config()
 twc_game_maker = TWCGameMaker(config)
 
+from textworld import GameMaker
+from textworld.generator.game import Event, Quest
+M = GameMaker()
+room = M.new_room('my room')
+shit = M.new('o', 'shit')
+shit_hole = M.new('c', 'shit hole')
+new_shit_hole = M.new('c', 'new shit hole')
+shit_hole.add(shit)
+room.add(shit_hole)
+room.add(new_shit_hole)
+M.set_player(room)
+# quest1 = ['open shit hole', 'take shit', 'open new shit hole', 'insert shit into new shit hole']
+# insert_shit = M.new_event_using_commands(quest1)
+win_event = Event(conditions={M.new_fact("in", shit, new_shit_hole)})
+quest1 = Quest(win_events=[win_event],
+               reward=2)
+M.quests = [quest1]
+
+
+
+
