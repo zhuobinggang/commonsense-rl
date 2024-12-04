1dc5493456342dac8b00089ffdc19e151dec8410
zhuobinggang <zhuobinggang@gmail.com>
2024-12-04 13:38:55 +0900

    dd

---
+++ action_selector.py
@@ -0,0 +1,54 @@
+from llm_simplify import quest_simple_get_text
+
+class Action_select_promptor:
+    def __init__(self):
+        self.system_msg = ''
+        self.user_msg = ''
+        self.prompt = ''
+        self.target_action = ''
+        self.action_list = []
+
+    def action_list_text(self, action_list):
+        return '[' + ', '.join(action_list) + ']'
+
+    def build(self):
+        system_msg = ''
+        system_msg += 'Task: Select the action from the list that is semantically closest to the target action.\n'
+        system_msg = system_msg.strip() + '\n'
+        self.system_msg = system_msg
+        user_msg = ''
+        user_msg += f'Target Action: {self.target_action}\n'
+        user_msg += f'Action List: {self.action_list_text(self.action_list)}\n'
+        user_msg += 'Answer (action closest to the target action):\n'
+        self.user_msg = user_msg
+        self.prompt = f'{system_msg}{user_msg}'
+
+    def sys_usr_msg(self):
+        return self.system_msg, self.user_msg
+
+def sys_usr_from_actions_and_action(action_list, action):
+    promptor = Action_select_promptor()
+    promptor.target_action = action
+    promptor.action_list = action_list
+    promptor.build()
+    return promptor.sys_usr_msg()
+
+def test_promptor():
+    action = 'examine table'
+    action_list = ['eat book', 'drink food']
+    sys, usr = sys_usr_from_actions_and_action(action_list, action)
+    print(sys + usr)
+
+def quest_closet_action(action_list, action):
+    if len(action_list) < 1:
+        return None
+    if not action:
+        return None
+    sys, usr = sys_usr_from_actions_and_action(action_list, action)
+    answer = quest_simple_get_text(sys, usr)
+    answer = answer.strip()
+    if answer not in [action_list]:
+        print('好像没有找到最相近的命令，应该考虑在action history加上类似的提醒。')
+        print(answer)
+    return answer
+    
--- exp/finetune_4omini_simplify_actions/README.md
+++ exp/finetune_4omini_simplify_actions/README.md
@@ -7,3 +7,21 @@
 > Action history: Action 0: hang wet green T-shirt on clothesline -> ("That's not a verb I recognise.\n",) Action 1: put wet green T-shirt on clothesline -> You put the wet green T-shirt on the clothesline.Your score has just gone up by one point. Action 2: take wet black shirt from patio table -> You take the wet black shirt from the patio table. Action 3: put wet black shirt on clothesline -> You put the wet black shirt on the clothesline.Your score has just gone up by one point. Action 4: take wet gray dress from patio chair -> You take the wet gray dress from the patio chair. Action 5: put wet gray dress on clothesline -> You put the wet gray dress on the clothesline.Your score has just gone up by one point. Action 6: take wet plaid blazer from floor -> ("You can't see any such thing.\n",) Action 7: take wet striped blazer from floor -> ("You can't see any such thing.\n",) Action 8: take wet plaid blazer from floor -> ("You can't see any such thing.\n",) 
 
 -> 解决方案：实现一个action selector，从环境列表中获取最接近的action
+
+## Action selector的prompt
+
+```
+Task: 请从行动列表中选择与目标行动语义上最接近的一个。
+目标行动：
+行动列表： action 1, action 2
+回答(与目标行动最接近的行动)：
+```
+
+```
+Task: Please select the action from the list that is semantically closest to the target action.
+Target Action:
+Action List: action 1, action 2
+Answer (Action closest to the target action):
+```
+
+## Action selector -> action_selector.py
